297fa2145491636bd1309a87a8c967a932f2e248
Dakkshesh <dakkshesh5@gmail.com>
2022-12-15 20:43:20 +0530

    docker: Fedora moment
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Base Image
-FROM archlinux:base-devel
+FROM fedora:latest
 
 # User
 USER root
@@ -7,31 +7,9 @@ USER root
 # Working Directory
 WORKDIR /root
 
-ENV LANG=en_US.UTF-8
 CMD ["/usr/bin/bash"]
 
-# Remove Files before copying the Rootfs
-COPY remove /tmp/
-RUN rm -rf $(< /tmp/remove)
-
-# Copy Rootfs and Scripts
-COPY rootfs /
-
 # Install Packages
-COPY pre_install.sh /tmp/
-RUN bash /tmp/pre_install.sh
-
-COPY archlinux_packages.sh /tmp/
-RUN bash /tmp/archlinux_packages.sh
-
-COPY alhp_packages.sh /tmp/
-RUN bash /tmp/alhp_packages.sh
-
-COPY custom_packages.sh /tmp/
-RUN bash /tmp/custom_packages.sh
-
-COPY post_install.sh /tmp/
-RUN bash /tmp/post_install.sh
-
-# Remove the Scripts we used
-RUN rm -rf /tmp/{{pre_install.sh,archlinux_packages,alhp_packages,custom_packages,post_install}.sh,remove}
+COPY setup.sh /tmp/
+RUN bash /tmp/setup.sh
+RUN rm -rf /tmp/setup.sh
---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Fedora Package installtion & setup
+set -e
+
+# dnf configuration
+{
+    echo "max_parallel_downloads=20"
+    echo "fastestmirror=True"
+    echo "defaultyes=True"
+} >>/etc/dnf/dnf.conf
+
+# Enable RPM Fusion Repos
+dnf install -y \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+# Update packages
+dnf update -y
+dnf upgrade -y
+
+# Install required packages
+dnf install -y \
+    bc \
+    binutils-devel \
+    bison \
+    ccache \
+    clang \
+    cmake \
+    elfutils-libelf-devel \
+    flex \
+    gcc \
+    gcc-c++ \
+    git \
+    git \
+    lld \
+    make \
+    nano \
+    neofetch \
+    ninja-build \
+    openssl-devel \
+    python3 \
+    python3-pip \
+    sudo \
+    texinfo-tex \
+    tmate \
+    uboot-tools \
+    xz \
+    zlib-devel
+
+# pip packages
+pip3 install \
+    telegram-send
+
+# git configuration
+git config --global user.name "Dakkshesh"
+git config --global user.email "dakkshesh5gmail.com"
+git config --global color.ui auto
+
+# Timezone configuration
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+timedatectl set-timezone Asia/Kolkata
+
+echo 'Fedora packages installtion completed'
