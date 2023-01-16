60438e0cd41316544dbca690faeed225ccf80242
Dakkshesh <dakkshesh5@gmail.com>
2023-01-16 20:49:36 +0530

    cos_packages: Dont enable x86-64-v4 repos

--- cos_packages.sh
+++ cos_packages.sh
@@ -6,7 +6,10 @@ set -e
 wget "https://mirror.cachyos.org/cachyos-repo.tar.xz"
 tar -xf "cachyos-repo.tar.xz"
 cd cachyos-repo
+# Pass `--noconfirm` to make it work in CI runs.
 sed -i 's|pacman -U|pacman -U  --noconfirm|' cachyos-repo.sh
+# Dont enable x86-64-v4 repos.
+sed -i '/add_specific_repo x86-64-v4 */d' cachyos-repo.sh 
 bash cachyos-repo.sh
 cd ..
 rm -rf cachyos-repo
